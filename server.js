3c3cec97f7b175d81978433d8abeb9cd6e2aefff
Ariful islam <arifulbgt4@gmail.com>
2024-09-14 16:23:04 +0600

    refactor: ♻️ security checkup

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const handlebars = require("handlebars");
+const cors = require("cors");
 
 const { authenticate } = require("./middlewares/authMiddleware");
 const { sendEmail } = require("./controllers/sendEmail");
@@ -8,6 +9,17 @@ const { sendEmail } = require("./controllers/sendEmail");
 // Express setup
 const app = express();
 app.use(express.json());
+
+const corsOptions = {
+  origin: process.env.ORIGIN, // Allow only this origin
+  methods: "GET,POST,PUT,DELETE", // Allow only these methods
+  allowedHeaders: "Content-Type,x-auth-iv,x-auth-encrypt,x-auth-tag", // Allow these headers
+  credentials: true, // Allow credentials if necessary
+};
+
+app.use(cors(corsOptions));
+// Handle pre-flight OPTIONS requests
+app.options("*", cors(corsOptions));
 
 const MAX_CAPACITY = process.env.MAX_CAPACITY || 5;
 
@@ -28,7 +40,9 @@ app.post("/", authenticate, async (req, res) => {
   const response = await sendEmail({ to, subject, htmlContent });
   console.log("send res: ", to, " : ", response);
 
-  return res.status(200).json({ message: `Successfully send email to: ${to}` });
+  return res
+    .status(200)
+    .json({ message: `Successfully send email to: ${to}`, response });
 });
 
 // Endpoint multiple email send
@@ -54,6 +68,7 @@ app.post("/multiple", authenticate, async (req, res) => {
 
   // Compile the Handlebars template
   const compiledTemplate = handlebars.compile(template);
+  const responses = [];
 
   for (const recipient of recipients) {
     const htmlContent = compiledTemplate(recipient?.props);
@@ -63,9 +78,13 @@ app.post("/multiple", authenticate, async (req, res) => {
       htmlContent,
     });
     console.log("send res: ", recipient?.to, " : ", response);
+
+    responses.push(response);
   }
 
-  return res.status(200).json({ message: `Successfully send emails` });
+  return res
+    .status(200)
+    .json({ message: `Successfully send emails`, responses });
 });
 
 // Start the server
